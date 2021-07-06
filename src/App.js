c3f29158ef9dc699dd9ffac49c4a9ad04f26267a
github-VladOrpik <65450885+VladOrpik@users.noreply.github.com>
2021-07-06 13:55:51 +0300

    create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
 import './App.css';
 import Header from './components/Header';
 import Filter from './components/filter';
+import TaskCreate from './components/taskCreate';
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Filter/>
+     <TaskCreate/>
     </div>
   );
 }
